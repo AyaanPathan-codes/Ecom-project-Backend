3a61e8fc949862eb1d0b4003588580bf7fa08b4a
Ayaan Pathan <ayaanpathanra@gmail.com>
2026-04-25 15:44:08 +0530

    Add User Controller

---
+++ src/main/java/com/ayaan/Ecom_project_1/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.ayaan.Ecom_project_1.controller;
+
+import com.ayaan.Ecom_project_1.model.User;
+import com.ayaan.Ecom_project_1.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userService.getUser(id);
+    }
+}
