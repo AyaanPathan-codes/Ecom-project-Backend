c6e65971045d0ee39ebfa4bc04ef9fa7a3206ca3
Ayaan Pathan <ayaanpathanra@gmail.com>
2026-04-25 15:32:28 +0530

    Add Order Repo

---
+++ src/main/java/com/ayaan/Ecom_project_1/repository/OrderItem.java
@@ -0,0 +1,6 @@
+package com.ayaan.Ecom_project_1.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItem extends JpaRepository<OrderItem,Long> {
+}
